3ef119976958e5dff02730feb1e9fa60d8d7e9d5
Gyeonghyo Min <gyeonghyo@devsisters.com>
2019-04-26 15:24:03 +0900

    enum 읽기 추가

--- StaticDataManagerGenerator/main.cpp
+++ StaticDataManagerGenerator/main.cpp
@@ -33,26 +33,24 @@ std::regex elem_list("\\s*(\\w+)\\s+@\\d+\\s*:\\s*List\\((\\w+)\\)\\s*;\\s*");  
 std::regex serverOnly(".*\\$Data.serverOnly;\\s*");                                 // itemHardLimits           @3 :List(ItemData) $Data.serverOnly;
 std::regex closingBrace("\\s*\\}\\s*");                                             // }
 std::regex enum_usual("\\s*enum\\s+(\\w+)\\s*\\{\\s*");                             // enum Source {
-std::regex elem_enum("\\s*(\\w+)\\s+@\\d+\\s*");                                    // none @0;
+std::regex elem_enum("\\s*(\\w+)\\s+@\\d+\\s*;\\s*");                               // none @0;
 std::regex whiteSpace("\\s*");                                                      //
 
 /* key - capnpName, value - cppName */
 std::map<std::string, std::string> convertName_baseTypes;           // 기본 타입 (int, string)
 /* key - scopeName, value - cppName */
 std::map<std::string, std::string> convertName_enumTypes;           // enum 타입
-//std::map<std::string, std::string> convertName_structTypes;        // struct 타입
+/* elem - scopeName == cppName */
+std::set<std::string> existing_structTypes;                          // struct 타입
 
 /* key - cppName, value - cppName */
-std::map<std::string, std::string> keyNames;                     // GB.Key가 있는 타입
+std::map<std::string, std::string> keyNames;                        // GB.Key
 std::map<std::string, std::deque<std::pair<std::string, std::string>>> members;   //pair<capnpName, cppType>
 std::map<std::string, std::deque<std::pair<std::string, std::string>>> listTypeMembers;   //pair<capnpName, cppType>
 
-bool isBaseType(std::string typeName)
-{
-    return convertName_baseTypes.count(typeName) != 0;
-}
+std::string rootName;
 
-bool isEnumType(std::string typeName)
+bool isBaseType(std::string typeName)
 {
     return convertName_baseTypes.count(typeName) != 0;
 }
@@ -90,12 +88,60 @@ std::string getKey(std::deque<std::string>& parents, std::string name = "")
     return ret;
 }
 
-void readEnum(std::ifstream& input, std::deque<std::string>& parents, std::string name, int& linenum)
+std::string getCppNameSpace(std::deque<std::string>& parents)
 {
-    
+    std::string ret = "gb::capnp::gamedata";
+    for(auto st : parents)
+    {
+        ret += "::";
+        ret += st;
+    }
+    return ret;
 }
 
-void readStruct(std::ifstream& input, std::deque<std::string>& parents, std::string name, int& linenum)
+//단순히 enum을 convertName_enumTypes 에 저장
+void readEnum(std::ifstream& input, std::deque<std::string>& parents, std::string name, int& linenum)
+{
+    std::string dir = getKey(parents, name);
+    parents.push_back(name);
+    
+    //실행 목적: enum의 위치와 cpp 이름을 파악하기 위해
+    convertName_enumTypes[dir] = getCppNameSpace(parents);
+    
+    if(MY_DEBUG >= 2) std::cout << "enum ns: " << getCppNameSpace(parents) << '\n';
+    
+    std::string line;
+    while(getline(input, line))
+    {
+        linenum++;
+        std::smatch matches;
+        
+        if(std::regex_match(line, matches, whiteSpace) ||
+           std::regex_match(line, matches, comment)    ||
+           std::regex_match(line, matches, elem_enum))
+        {
+            continue;
+        }
+        else if(std::regex_match(line, matches, closingBrace))
+        {
+            if(MY_DEBUG == 2) std::cout << "closingBrace" << '\n';
+            parents.pop_back();
+            return;
+        }
+        else
+        {
+            std::cout << "ERROR: Unknown pattern found, failed to parse line " << linenum << ".\n";
+            std::cout << "The line at fault: \n" << line << "\n\n";
+            return;
+        }
+    }
+    
+    std::cout << "ERROR: Unexpected end of file at line " << linenum << ".\n";
+    return;
+}
+
+//2회차: 파일을 읽으며 멤버 변수들을 저장
+void readElem(std::ifstream& input, std::deque<std::string>& parents, std::string name, int& linenum)
 {
     std::string parentDir = getKey(parents);
     std::string dir = getKey(parents, name);
@@ -112,17 +158,23 @@ void readStruct(std::ifstream& input, std::deque<std::string>& parents, std::str
         std::smatch matches;
         
         if(std::regex_match(line, matches, whiteSpace) ||
-           std::regex_match(line, matches, comment))
+           std::regex_match(line, matches, comment)    ||
+           std::regex_match(line, matches, serverOnly))
         {
             continue;
         }
         else if(std::regex_match(line, matches, struct_usual))
         {
-            readStruct(input, parents, matches[1].str(), linenum);
+            readElem(input, parents, matches[1].str(), linenum);
             if(MY_DEBUG == 2) std::cout << "struct_usual" << '\n';
         }
+        else if(std::regex_match(line, matches, enum_usual))
+        {
+            readEnum(input, parents, matches[1].str(), linenum);
+        }
         else if(std::regex_match(line, matches, elem_key) ||
-                std::regex_match(line, matches, elem))
+                std::regex_match(line, matches, elem)     ||
+                std::regex_match(line, matches, serverOnly))
         {
             std::string cppKeyType = "";
             std::string keyType = matches[2].str();
@@ -148,7 +200,7 @@ void readStruct(std::ifstream& input, std::deque<std::string>& parents, std::str
             
             members[dir].push_back(make_pair(matches[1].str(), cppKeyType));
             if(std::regex_match(line, matches, elem_key))
-               keyNames[dir] = cppKeyType;
+                keyNames[dir] = cppKeyType;
             
             
             if(MY_DEBUG == 2) std::cout << "elem" << '\n';
@@ -161,10 +213,6 @@ void readStruct(std::ifstream& input, std::deque<std::string>& parents, std::str
         else if(std::regex_match(line, matches, elem_list))
         {
             if(MY_DEBUG == 2) std::cout << "elem_list" << '\n';
-        }
-        else if(std::regex_match(line, matches, serverOnly))
-        {
-            if(MY_DEBUG == 2) std::cout << "serverOnly" << '\n';
         }
         else if(std::regex_match(line, matches, closingBrace))
         {
@@ -180,7 +228,66 @@ void readStruct(std::ifstream& input, std::deque<std::string>& parents, std::str
         }
     }
     
-    parents.pop_back();
+    std::cout << "ERROR: Unexpected end of file at line " << linenum << ".\n";
+    return;
+}
+
+//1회차: 파일을 읽으며 구조체와 enum의 hierarchy 파악
+void readStruct(std::ifstream& input, std::deque<std::string>& parents, std::string name, int& linenum)
+{
+    std::string parentDir = getKey(parents);
+    std::string dir = getKey(parents, name);
+    
+    parents.push_back(name);
+    
+    //readStruct 의 목적1 - struct의 위치를 알아내고 existing_structTypes 에 저장
+    existing_structTypes.insert(dir);
+    
+    if(MY_DEBUG == 2) std::cout << "pDir: " << parentDir << '\n';
+    if(MY_DEBUG == 2) std::cout << "dir: " << dir << '\n';
+    
+    std::string line;
+    while(getline(input, line))
+    {
+        linenum++;
+        std::smatch matches;
+        
+        if(std::regex_match(line, matches, whiteSpace) ||
+           std::regex_match(line, matches, comment))
+        {
+            continue;
+        }
+        else if(std::regex_match(line, matches, struct_usual))
+        {
+            readStruct(input, parents, matches[1].str(), linenum);
+            if(MY_DEBUG == 2) std::cout << "struct_usual" << '\n';
+        }
+        else if(std::regex_match(line, matches, elem_key)   ||
+                std::regex_match(line, matches, elem)       ||
+                std::regex_match(line, matches, elem_list)  ||
+                std::regex_match(line, matches, serverOnly))
+        {
+            continue;
+        }
+        else if(std::regex_match(line, matches, enum_usual))
+        {
+            readEnum(input, parents, matches[1].str(), linenum);
+        }
+        else if(std::regex_match(line, matches, closingBrace))
+        {
+            if(MY_DEBUG == 2) std::cout << "closingBrace" << '\n';
+            parents.pop_back();
+            return;
+        }
+        else
+        {
+            std::cout << "ERROR: Unknown pattern found, failed to parse line " << linenum << ".\n";
+            std::cout << "The line at fault: \n" << line << "\n\n";
+            return;
+        }
+    }
+    
+    std::cout << "ERROR: Unexpected end of file at line " << linenum << ".\n";
     return;
 }
 
@@ -206,6 +313,7 @@ int main(int argc, const char * argv[])
     std::string line;
     int linenum = 0;
     
+    /* import 처리 + 1회차 구조 파악 */
     while(getline(input, line))
     {
         linenum++;
@@ -246,8 +354,16 @@ int main(int argc, const char * argv[])
                     std::cout << i << ": '" << matches[i].str() << "'\n";
                 }
             }
+            if(std::regex_match(line, matches, struct_root))
+               rootName = matches[1].str();
+            
             std::deque<std::string> parents = std::deque<std::string>();
             readStruct(input, parents, matches[1].str(), linenum);
+        }
+        else if(std::regex_match(line, matches, enum_usual))
+        {
+            std::deque<std::string> parents = std::deque<std::string>();
+            readEnum(input, parents, matches[1].str(), linenum);
         }
         else
         {
@@ -260,6 +376,7 @@ int main(int argc, const char * argv[])
     input = std::ifstream(argv[1]);
     linenum = 0;
     
+    /* 2회차 - 멤버 변수 파악 */
     while(getline(input, line))
     {
         linenum++;
@@ -283,7 +400,12 @@ int main(int argc, const char * argv[])
                 }
             }
             std::deque<std::string> parents = std::deque<std::string>();
-            readStruct(input, parents, matches[1].str(), linenum);
+            readElem(input, parents, matches[1].str(), linenum);
+        }
+        else if(std::regex_match(line, matches, enum_usual))
+        {
+            std::deque<std::string> parents = std::deque<std::string>();
+            readEnum(input, parents, matches[1].str(), linenum);
         }
         else
         {
