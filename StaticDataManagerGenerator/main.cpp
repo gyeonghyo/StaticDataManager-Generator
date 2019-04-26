687c66421045c8064ff67cd608022ccdb2612e19
Gyeonghyo Min <gyeonghyo@devsisters.com>
2019-04-26 17:58:58 +0900

    import 지원 기능 추가

--- StaticDataManagerGenerator/main.cpp
+++ StaticDataManagerGenerator/main.cpp
@@ -16,32 +16,38 @@
 #include <map>
 #include <set>
 #include <deque>
+#include <sstream>
 
 #define MY_DEBUG 3
 #define LINES_TO_IGNORE 11
 
 //using namespace std;
 
-std::regex comment("\\s*#.*");                                                      // # 시즌 종료 후 정산하는데 쓸 여유 시간
-std::regex import("\\s*using\\s+(\\w+)\\s*=\\s*import\\s+([\"./\\w]+)\\s*;\\s*");   // using Action = import "achievements.capnp".Action;
-std::regex import2("\\s*using\\s+import\\s+([\"./\\w]+);\\s*");                     // using import "events.capnp".MessageReward;
-std::regex struct_root("\\s*struct\\s+(\\w+)\\s+\\$Data.root\\s*\\{\\s*");          // struct MysteryMazeMode $Data.root {
-std::regex struct_usual("\\s*struct\\s+(\\w+)\\s*\\{\\s*");                         // struct Season {
-std::regex elem_key("\\s*(\\w+)\\s+@\\d+\\s*:\\s*(\\w+)\\s+\\$GB.key\\s*;\\s*");    // id                       @0 :Int16 $GB.key;
-std::regex elem("\\s*(\\w+)\\s+@\\d+\\s*:\\s*(\\w+);\\s*");                         // startTime                @1 :Int64;
-std::regex elem_list("\\s*(\\w+)\\s+@\\d+\\s*:\\s*List\\((\\w+)\\)\\s*;\\s*");      // mazeTables               @2 :List(MazeTable);
-std::regex serverOnly(".*\\$Data.serverOnly;\\s*");                                 // itemHardLimits           @3 :List(ItemData) $Data.serverOnly;
-std::regex closingBrace("\\s*\\}\\s*");                                             // }
-std::regex enum_usual("\\s*enum\\s+(\\w+)\\s*\\{\\s*");                             // enum Source {
-std::regex elem_enum("\\s*(\\w+)\\s+@\\d+\\s*;\\s*");                               // none @0;
-std::regex whiteSpace("\\s*");                                                      //
+std::regex comment("\\s*#.*");                                                          // # 시즌 종료 후 정산하는데 쓸 여유 시간
+std::regex import("\\s*using\\s+(\\w+)\\s*=\\s*import\\s+([\"./\\w]+)\\s*;\\s*");       // using Action = import "achievements.capnp".Action;
+std::regex import2("\\s*using\\s+import\\s+([\"./\\w]+);\\s*");                         // using import "events.capnp".MessageReward;
+std::regex struct_root("\\s*struct\\s+(\\w+)\\s+\\$Data.root\\s*\\{\\s*");              // struct MysteryMazeMode $Data.root {
+std::regex struct_usual("\\s*struct\\s+(\\w+)\\s*\\{\\s*");                             // struct Season {
+std::regex elem_key("\\s*(\\w+)\\s+@\\d+\\s*:\\s*([\\.\\w]+)\\s+\\$GB.key\\s*;\\s*");   // id                       @0 :Int16 $GB.key;
+std::regex elem("\\s*(\\w+)\\s+@\\d+\\s*:\\s*([\\.\\w]+);\\s*");                        // startTime                @1 :Int64;
+std::regex elem_list("\\s*(\\w+)\\s+@\\d+\\s*:\\s*List\\(([\\.\\w]+)\\)\\s*;\\s*");     // mazeTables               @2 :List(MazeTable);
+std::regex serverOnly(".*\\$Data.serverOnly;\\s*");                                     // itemHardLimits           @3 :List(ItemData) $Data.serverOnly;
+std::regex closingBrace("\\s*\\}\\s*");                                                 // }
+std::regex enum_usual("\\s*enum\\s+(\\w+)\\s*\\{\\s*");                                 // enum Source {
+std::regex elem_enum("\\s*(\\w+)\\s+@\\d+\\s*;\\s*");                                   // none @0;
+std::regex whiteSpace("\\s*");                                                          //
 
 /* key - capnpName, value - cppName */
 std::map<std::string, std::string> convertName_baseTypes;           // 기본 타입 (int, string)
+/* key - capnpName, value - cppName */
+std::map<std::string, std::string> convertName_importedTypes;       // import 된 타입
 /* key - scopeName, value - cppName */
 std::map<std::string, std::string> convertName_enumTypes;           // enum 타입
 /* elem - scopeName == cppName */
-std::set<std::string> existing_structTypes;                          // struct 타입
+std::set<std::string> existing_structTypes;                         // struct 타입
+
+/* elem - cppName */
+std::set<std::string> existing_importedTypes;                       // .cpp 에 생성된 imported 타입
 
 /* key - cppName, value - cppName */
 std::map<std::string, std::string> keyNames;                        // GB.Key
@@ -59,6 +65,8 @@ bool isBaseType(std::string typeName)
 void insertBaseTypes()
 {
     convertName_baseTypes["Text"] = "std::string";
+    convertName_baseTypes["Bool"] = "bool";
+    convertName_baseTypes["Float32"] = "float";
     convertName_baseTypes["Int64"] = "int64_t";
     convertName_baseTypes["Int32"] = "int32_t";
     convertName_baseTypes["Int16"] = "int16_t";
@@ -174,8 +182,9 @@ void readElem(std::ifstream& input, std::deque<std::string>& parents, std::strin
         }
         else if(std::regex_match(line, matches, elem_key) ||
                 std::regex_match(line, matches, elem)     ||
-                std::regex_match(line, matches, serverOnly))
+                std::regex_match(line, matches, elem_list))
         {
+            //capnp의 변수명을 cpp의 변수명으로 변환 후 멤버 변수 리스트에 저장
             std::string cppKeyType = "";
             std::string keyType = matches[2].str();
             if(isBaseType(keyType))
@@ -184,13 +193,53 @@ void readElem(std::ifstream& input, std::deque<std::string>& parents, std::strin
             }
             else
             {
-                if(convertName_enumTypes.count(dir + '_' + keyType) != 0)
+                std::regex hasDot("(\\w+)\\.(\\w+)");
+                std::smatch matches_dot;
+                std::string nestedType;
+                bool has_Dot = false;
+                if(std::regex_match(keyType, matches_dot, hasDot))
                 {
-                    cppKeyType = convertName_enumTypes[parentDir + '_' + keyType];
+                    has_Dot = true;
+                    keyType = matches_dot[1].str();
+                    nestedType = matches_dot[2].str();
                 }
-                else if(convertName_enumTypes.count(parentDir + '_' + keyType) != 0)
+                
+                std::string siblingLocation = parentDir.compare("") != 0 ? parentDir + '_' + keyType : keyType;
+                std::string childLocation = dir.compare("") != 0 ? dir + '_' + keyType : keyType;
+                
+                //import는 절대위치만 확인, enum 이나 struct 면 스코프를 확인 후 변환
+                if(convertName_importedTypes.count(keyType) != 0)
                 {
-                    cppKeyType = convertName_enumTypes[dir + '_' + keyType];
+                    cppKeyType = convertName_importedTypes[keyType];
+                    if(has_Dot)
+                        cppKeyType += "::" + nestedType;
+                    
+                    if(MY_DEBUG == 3)
+                        std::cout << "line: " << linenum << " cppType: " << cppKeyType << '\n';
+                }
+                else if(convertName_enumTypes.count(childLocation) != 0)
+                {
+                    cppKeyType = convertName_enumTypes[childLocation];
+                    if(has_Dot)
+                        cppKeyType += "::" + nestedType;
+                }
+                else if(convertName_enumTypes.count(siblingLocation) != 0)
+                {
+                    cppKeyType = convertName_enumTypes[siblingLocation];
+                    if(has_Dot)
+                        cppKeyType += "::" + nestedType;
+                }
+                else if(existing_structTypes.count(childLocation) != 0)
+                {
+                    cppKeyType = childLocation;
+                    if(has_Dot)
+                        cppKeyType += '_' + nestedType;
+                }
+                else if(existing_structTypes.count(siblingLocation) != 0)
+                {
+                    cppKeyType = siblingLocation;
+                    if(has_Dot)
+                        cppKeyType += '_' + nestedType;
                 }
                 else
                 {
@@ -198,21 +247,23 @@ void readElem(std::ifstream& input, std::deque<std::string>& parents, std::strin
                 }
             }
             
-            members[dir].push_back(make_pair(matches[1].str(), cppKeyType));
-            if(std::regex_match(line, matches, elem_key))
+            //리스트 타입이면 listTypeMembers, 아니면 members 에 멤버 변수 추가
+            if(std::regex_match(line, elem_list))
+                listTypeMembers[dir].push_back(make_pair(matches[1].str(), cppKeyType));
+            else
+                members[dir].push_back(make_pair(matches[1].str(), cppKeyType));
+            
+            //만일 key 타입이면 keyName에 추가
+            if(std::regex_match(line, elem_key))
                 keyNames[dir] = cppKeyType;
             
             
             if(MY_DEBUG == 2) std::cout << "elem" << '\n';
-            if(MY_DEBUG == 3)
+            if(MY_DEBUG == -1)
             {
                 std::cout << members[dir].begin()->first << '\n';
                 std::cout << members[dir].begin()->second << '\n';
             }
-        }
-        else if(std::regex_match(line, matches, elem_list))
-        {
-            if(MY_DEBUG == 2) std::cout << "elem_list" << '\n';
         }
         else if(std::regex_match(line, matches, closingBrace))
         {
@@ -313,26 +364,74 @@ int main(int argc, const char * argv[])
     std::string line;
     int linenum = 0;
     
-    /* import 처리 + 1회차 구조 파악 */
+    /* import 처리 + 1회차: hierarchy 파악 */
     while(getline(input, line))
     {
         linenum++;
         std::smatch matches;
         if(linenum <= LINES_TO_IGNORE               ||
            std::regex_match(line, matches, comment) ||
-           std::regex_match(line, matches, whiteSpace)) //주석
+           std::regex_match(line, matches, whiteSpace)) //무시 대상
         {
             continue;
         }
         else if(std::regex_match(line, matches, import)) //import
         {
-            if(MY_DEBUG == 2)
+            std::string myName = matches[1].str();
+            std::string toImport = matches[2].str();
+            
+            std::regex hasDot("([\"./\\w]+)\\.(\\w+)");
+            std::smatch matches_dot;
+            std::string nestedType;
+            bool has_Dot = false;
+            if(std::regex_match(toImport, matches_dot, hasDot))
             {
-                std::cout << 1 << '\n';
-                for (size_t i = 0; i < matches.size(); ++i) {
-                    std::cout << i << ": '" << matches[i].str() << "'\n";
-                }
+                has_Dot = true;
+                toImport = matches_dot[1].str();
+                nestedType = matches_dot[2].str();
             }
+            
+            std::regex clean("\"([./\\w]+)\\.capnp\"");
+            std::smatch cleanResult;
+            if(std::regex_match(toImport, cleanResult, clean))
+            {
+                toImport = cleanResult[1].str();
+            }
+            else
+            {
+                std::cout << "ERROR: Unknown pattern found, failed to parse line " << linenum << ".\n";
+                std::cout << "The line at fault: \n" << line << "\n\n";
+                return 0;
+            }
+            
+            std::replace(toImport.begin(), toImport.end(), '/', ' ');
+            std::vector<std::string> array;
+            std::stringstream ss(toImport);
+            std::string token;
+            while(ss >> token)
+                array.push_back(token);
+            
+            if(has_Dot)
+                array.push_back(nestedType);
+            
+            std::string cppType = "gb::capnp";
+            int p = 0;
+            if(array[0].compare("..") != 0)
+            {
+                cppType += "::gamedata";
+            }
+            else
+                p++;
+            
+            for(; p < array.size() - 1; p++)
+            {
+                cppType += "::" + array[p];
+            }
+            
+            convertName_importedTypes[myName] = cppType;
+            
+            if(MY_DEBUG == 3)
+                std::cout << "myName: " << myName << " cppType: " << cppType << '\n';
         }
         else if(std::regex_match(line, matches, import2)) //import2
         {
@@ -347,14 +446,8 @@ int main(int argc, const char * argv[])
         else if(std::regex_match(line, matches, struct_root) ||
                 std::regex_match(line, matches, struct_usual)) //struct_root
         {
-            if(MY_DEBUG == 2)
-            {
-                std::cout << "struct_root" << '\n';
-                for (size_t i = 0; i < matches.size(); ++i) {
-                    std::cout << i << ": '" << matches[i].str() << "'\n";
-                }
-            }
-            if(std::regex_match(line, matches, struct_root))
+            //루트 struct면 rootName 에 추가
+            if(std::regex_match(line, struct_root))
                rootName = matches[1].str();
             
             std::deque<std::string> parents = std::deque<std::string>();
